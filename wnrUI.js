93274340c8e1cfa1ecaadb0811ee12d32ca8563b
Timur Fatykhov <tfatykhov@gmail.com>
2016-06-06 19:58:02 -0400

    Update wnrUI.js

--- wnrUI.js
+++ wnrUI.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Urbiworx
+ * Copyright 2016 tfatykhov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
